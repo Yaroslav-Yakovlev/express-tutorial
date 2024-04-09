368f191b4f33c483bfcc3011a0a6dfc63e92580e
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-04-09 16:05:47 +0300

    added express-session

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,29 @@
 import express from 'express';
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser('helloworld'));
+
+app.use(session({
+  secret: 'test',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 60000 * 60,
+  },
+}));
 app.use(routes);
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
+  console.log(req.session);
+  console.log(req.session.id);
+  req.session.visited = true;
   res.cookie('hello', 'world', { maxAge: 30000, signed: true });
   res.status(201).send({ msg: 'Hello' });
 });
